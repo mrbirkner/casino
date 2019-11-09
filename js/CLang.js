a80e40ce8cff95aed934b0c6ed81cb86f475349e
mrbirkner <57419393+mrbirkner@users.noreply.github.com>
2019-11-09 09:58:35 +0100

    Add files via upload

---
+++ js/CLang.js
@@ -0,0 +1,22 @@
+var TEXT_MONEY     = "MONEY";
+var TEXT_PLAY      = "LETS GO";
+var TEXT_BET       = "BET";
+var TEXT_COIN      = "COIN";
+var TEXT_MAX_BET   = "FULL DEGEN";
+var TEXT_INFO      = "INFO";
+var TEXT_LINES     = "LINES";
+var TEXT_SPIN      = "LETS GO";
+var TEXT_WIN       = "WIN";
+var TEXT_HELP_WILD = "THIS SYMBOL IS THE WILD IT REPLACE ALL OTHER SYMBOLS";
+var TEXT_CREDITS_DEVELOPED = "";
+var TEXT_CURRENCY = "$";
+var TEXT_PRELOADER_CONTINUE = "START";
+var TEXT_NO_MONEY = "NO MONEY! DO YOU WANT TO RECHARGE?";
+var TEXT_RECHARGE = "RECHARGE";
+
+var TEXT_SHARE_IMAGE = "200x200.jpg";
+var TEXT_SHARE_TITLE = "Congratulations!";
+var TEXT_SHARE_MSG1 = "You collected <strong>";
+var TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+var TEXT_SHARE_SHARE1 = "My score is ";
+var TEXT_SHARE_SHARE2 = " points! Can you do better?";
